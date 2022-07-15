ed61c41b2fd317b9e82881f7cb84f0270fd53e29
lenusky <92371955+lenusky@users.noreply.github.com>
2022-07-15 19:53:11 +0200

    Delete images.sql

--- images.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
-  id_usuario` int(11) NOT NULL,
-  `titulo` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  
-) ENGINE=InnoDB DEFAULT CHARSET=utf_general_ci;
