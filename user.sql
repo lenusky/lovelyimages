5f319179427261156a4a6b59667545fc7361f227
lenusky <92371955+lenusky@users.noreply.github.com>
2022-07-18 09:33:40 +0200

    Delete user.sql

--- user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf_general_ci AUTO_INCREMENT=1 ;
